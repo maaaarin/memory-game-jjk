64210a0d17ef5d954e9bcbe8f2522fb2ac8df2e8
Andrés Marín <andresfprivado@gmail.com>
2024-01-23 20:47:44 +0100

    final

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, {  useEffect, useRef, useState } from 'react'
+import React, {  useRef, useState } from 'react'
 import menuAudio from '../assets/sound/menu.mp3';
 
 const Menu = ( { gamePhase } ) => {
